25be36709c6584948850da59e5546d7c16ccc07e
Mine <103xzy@gmail.com>
2020-12-08 15:37:17 +0800

    poker game started v2

---
+++ src/com/fju/Poker.java
@@ -0,0 +1,10 @@
+package com.fju;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		PokerCard poker = new PokerCard(28);
+		System.out.println(poker.value);
+	}
+
+}
--- src/com/fju/PokerCard.java
+++ src/com/fju/PokerCard.java
@@ -1,5 +1,10 @@
 package com.fju;
 
 public class PokerCard {
-
+	 int value ;
+	 char type;
+	 int point;
+	 public PokerCard(int value) {
+		 this.value = value;
+	 }
 }
